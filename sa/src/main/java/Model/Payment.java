95358e7089c3aea8b721d7f45692151c11b1f93d
root <root@BOOS>
2018-02-23 10:26:56 +0100

    test für neue java klasse

---
+++ sa/src/main/java/Model/Payment.java
@@ -0,0 +1,10 @@
+package Model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+public class Payment {
+	
+}
